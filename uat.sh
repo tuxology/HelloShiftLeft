9bca4bc1fd6fec86508eed4213bb8f5809e5c2e0
Chetan Conikee <conikee@gmail.com>
2019-02-18 09:27:40 -0800

    changes for 0.1

--- uat.sh
+++ uat.sh
@@ -1,4 +1,4 @@
-sl run -- java -jar target/hello-shiftleft-0.0.2.jar &
+sl run -- java -jar target/hello-shiftleft-0.0.1.jar &
 sleep 60
 curl http://localhost:8081/customers/1
 curl http://localhost:8081/customers/
