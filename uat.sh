482eec50f1ee703f2871716b9d8cad8b9b8de92b
Chetan Conikee <conikee@gmail.com>
2019-02-25 11:59:09 -0800

    pip additions

--- uat.sh
+++ uat.sh
@@ -1,4 +1,3 @@
-pip3 install requests
 sl run -- java -jar target/hello-shiftleft-0.0.5.jar &
 sleep 60
 curl http://localhost:8081/customers/1
@@ -20,6 +19,8 @@ curl http://localhost:8081/account/1
 sleep 5
 curl http://localhost:8081/patients
 sleep 10
+sudo easy_install pip
+pip install requests
 python2 exploits/filewriteexploit.py http://localhost:8081/saveSettings testfile 3vilhax0r
 sleep 5
 python2 exploits/JavaSerializationExploit/src/main/java/exploit.py
