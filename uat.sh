f8f34d031cbf1e67d2d4f5196a5572c687c73948
Chetan Conikee <conikee@gmail.com>
2019-02-25 20:54:49 -0800

    revised scripts

--- uat.sh
+++ uat.sh
@@ -10,6 +10,7 @@ do
     curl http://localhost:8081/customers/1
     curl http://localhost:8081/customers/1
     curl http://localhost:8081/customers/1
+    sleep 30
 done
 sleep 10
 for i in {1..100}
@@ -21,11 +22,13 @@ do
     curl http://localhost:8081/account/4
     curl http://localhost:8081/account/5
     curl http://localhost:8081/account/1
+    sleep 30
 done
 sleep 5
 for i in {1..50}
 do
-curl http://localhost:8081/patients
+    curl http://localhost:8081/patients
+    sleep 30
 done
 sleep 10
 sudo apt update
@@ -33,7 +36,8 @@ sudo apt install -y python-pip python-dev
 pip install requests
 for i in {1..25}
 do
-python2 exploits/filewriteexploit.py http://localhost:8081/saveSettings testfile 3vilhax0r
+    python2 exploits/filewriteexploit.py http://localhost:8081/saveSettings testfile 3vilhax0r
+    sleep 30
 done
 sleep 5
 python2 exploits/JavaSerializationExploit/src/main/java/exploit.py
