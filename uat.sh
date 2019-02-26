72d246c6708a027baa3429863f3a0e9ffe11854c
Chetan Conikee <conikee@gmail.com>
2019-02-25 20:49:42 -0800

    revised scripts

--- uat.sh
+++ uat.sh
@@ -1,28 +1,40 @@
 sl run -- java -jar target/hello-shiftleft-0.0.5.jar &
-sleep 60
-curl http://localhost:8081/customers/1
-curl http://localhost:8081/customers/
-curl http://localhost:8081/customers/2
-curl http://localhost:8081/customers/
-curl http://localhost:8081/customers/1
-curl http://localhost:8081/customers/1
-curl http://localhost:8081/customers/1
-curl http://localhost:8081/customers/1
+sleep 30
+for i in {1..100}
+do
+    curl http://localhost:8081/customers/1
+    curl http://localhost:8081/customers/
+    curl http://localhost:8081/customers/2
+    curl http://localhost:8081/customers/
+    curl http://localhost:8081/customers/1
+    curl http://localhost:8081/customers/1
+    curl http://localhost:8081/customers/1
+    curl http://localhost:8081/customers/1
+done
 sleep 10
-curl http://localhost:8081/account
-curl http://localhost:8081/account/1
-curl http://localhost:8081/account/2
-curl http://localhost:8081/account/3
-curl http://localhost:8081/account/4
-curl http://localhost:8081/account/5
-curl http://localhost:8081/account/1
+for i in {1..100}
+do
+    curl http://localhost:8081/account
+    curl http://localhost:8081/account/1
+    curl http://localhost:8081/account/2
+    curl http://localhost:8081/account/3
+    curl http://localhost:8081/account/4
+    curl http://localhost:8081/account/5
+    curl http://localhost:8081/account/1
+done
 sleep 5
+for i in {1..50}
+do
 curl http://localhost:8081/patients
+done
 sleep 10
 sudo apt update
 sudo apt install -y python-pip python-dev
 pip install requests
+for i in {1..25}
+do
 python2 exploits/filewriteexploit.py http://localhost:8081/saveSettings testfile 3vilhax0r
+done
 sleep 5
 python2 exploits/JavaSerializationExploit/src/main/java/exploit.py
 sleep 20
